f66f489a32621e8bab3967b23dbdc8bcc3499ea0
Riboshom <riboshom@hotmail.com>
2016-03-27 01:09:11 -0400

    Preset partitions

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ var app = angular.module('herod', ['ui.bootstrap']);
 app.controller('EcuCtrl', function($scope) {
   $scope.ecu = ecu;
   $scope.Couleurs = Couleurs;
+  $scope.Partitions = Partitions;
 
   $scope.$watch('ecu.couleur', ecu.updateCouleur);
+  $scope.$watch('ecu.partition', ecu.updatePartition);
 });
--- js/ecu.js
+++ js/ecu.js
@@ -12,31 +12,37 @@ Couleurs = {
   Argent: '#ffffff'
 };
 
+Partitions = {
+	Plain: function(height, width) {
+		this.svg.parts.push(this.svg.rect(width, height).fill(this.couleur));
+	},
+	Parti: function(height, width) {
+		this.svg.parts.push(this.svg.rect(width/2, height).fill(this.couleur));
+		this.svg.parts.push(this.svg.rect(width/2, height).x(width/2).fill(this.couleurAlt));
+	},
+}
+
 function Ecu() {
   this.forme = Formes.EcuFrancais;
+  this.partition = Partitions.Plain;
   this.couleur = Couleurs.Azur;
+  this.couleurAlt = Couleurs.Gueules;
 
-  _.bindAll(this, 'init', 'updateCouleur');
+  _.bindAll(this, 'init', 'updatePartition', 'updateCouleur');
 }
 
 Ecu.prototype.init = function() {
+  // Initiallize surface
   this.svg = SVG('ecu');
   this.svg.viewbox(0, 0, 100, 110);
 
-  this.svg.parts = [];  
+  this.svg.parts = [];
   this.svg.ecu = this.svg.path(this.forme).size(100);
-  
-  var width=this.svg.ecu.width();
-  var height=this.svg.ecu.height();
-  
-  this.svg.parts.push(this.svg.rect(width/2, height).fill(this.couleur));
-  this.svg.parts.push(this.svg.rect(width/2, height).x(width/2).fill(Couleurs.Gueules));
-  
-  var clip = this.svg.clip().add(this.svg.ecu);
-  
-  this.svg.parts.forEach(function(part, idx, array) {
-    array[idx] = part.clipWith(clip);
-  }, this);
+
+  this.svg.clippath = this.svg.clip().add(this.svg.ecu);
+
+  // Define and clip partitions
+  this.updatePartition();
 };
 
 Ecu.prototype.updateCouleur = function() {
@@ -44,6 +50,25 @@ Ecu.prototype.updateCouleur = function() {
     this.svg.parts[0].fill(this.couleur);
   }
 };
+
+Ecu.prototype.updatePartition = function() {
+  console.log("Updated")
+  if (this.svg) {
+	this.svg.parts.forEach(function(part) {
+		part.remove();
+	})
+	this.svg.parts.length = 0;
+    this.partition.apply(this, [this.svg.ecu.height(), this.svg.ecu.width()]);
+
+	this.reclip();
+  }
+}
+
+Ecu.prototype.reclip = function() {
+  this.svg.parts.forEach(function(part, idx, array) {
+    array[idx] = part.clipWith(this.svg.clippath);
+  }, this);
+}
 
 ecu = new Ecu();
 $(document).ready(ecu.init);
