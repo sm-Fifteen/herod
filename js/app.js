8caa85bea19df2f6df8cfd2aed04748288d6876a
Riboshom <riboshom@hotmail.com>
2016-09-10 20:46:40 -0400

    Ditched svg.js in favor of paper.js, partial implementation

--- js/app.js
+++ js/app.js
@@ -1,10 +1,40 @@
 var app = angular.module('herod', ['ui.bootstrap']);
 
+app.directive("ecuViewport", ["$timeout", function($timeout) {
+    return {
+        restrict: 'A',
+        scope: true,
+        link: function(scope, element) {
+            function drawEcuOn(passedElement){
+                if(!scope.paper) {
+                    scope.paper = new paper.PaperScope();
+                    scope.paper.setup(passedElement);
+                }
+
+                if(scope.ecu) {
+                    scope.shape = scope.paper.PathItem.create(scope.ecu.forme);
+                    scope.shape.fitBounds(scope.paper.view.bounds);
+                    scope.shape.fillColor = scope.ecu.couleur;
+                }
+
+                paper = scope.paper;
+                paper.view.draw();
+            }
+
+            // Timeout with 0 delay, to wait for the DOM to render.
+            $timeout(drawEcuOn(element[0]), 0);
+
+            scope.$watch("ecu.couleur", function(newVal) {
+                scope.shape.fillColor = newVal;
+            });
+        }
+    }
+}]);
+
 app.controller('EcuCtrl', function($scope) {
-  $scope.ecu = ecu;
   $scope.Couleurs = Couleurs;
   $scope.Partitions = Partitions;
+  console.log($scope.Couleurs);
 
-  $scope.$watch('ecu.couleur', ecu.updateCouleur);
-  $scope.$watch('ecu.partition', ecu.updatePartition);
+  $scope.ecu = new Ecu();
 });
--- js/ecu.js
+++ js/ecu.js
@@ -1,36 +1,11 @@
-Formes = {
-  EcuFrancais: 'M0,0 L0,50 Q0,80 50,110 Q100,80 100,50 L100,0 z'
-};
-
-Couleurs = {
-  Gueules: '#e20909',
-  Azur: '#2b5df2',
-  Sable: '#000000',
-  Sinople: '#5ab532',
-  Pourpre: '#d576ad',
-  Or: '#fcef3c',
-  Argent: '#ffffff'
-};
-
-Partitions = {
-	Plain: function(height, width) {
-		this.svg.parts.push(this.svg.rect(width, height).fill(this.couleur));
-	},
-	Parti: function(height, width) {
-		this.svg.parts.push(this.svg.rect(width/2, height).fill(this.couleur));
-		this.svg.parts.push(this.svg.rect(width/2, height).x(width/2).fill(this.couleurAlt));
-	},
-}
-
 function Ecu() {
   this.forme = Formes.EcuFrancais;
   this.partition = Partitions.Plain;
   this.couleur = Couleurs.Azur;
   this.couleurAlt = Couleurs.Gueules;
-
-  _.bindAll(this, 'init', 'updatePartition', 'updateCouleur');
 }
 
+/*
 Ecu.prototype.init = function() {
   // Initiallize surface
   this.svg = SVG('ecu');
@@ -69,6 +44,4 @@ Ecu.prototype.reclip = function() {
     array[idx] = part.clipWith(this.svg.clippath);
   }, this);
 }
-
-ecu = new Ecu();
-$(document).ready(ecu.init);
+*/
